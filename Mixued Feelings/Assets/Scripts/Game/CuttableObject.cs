5883eb89f3d419b5b1fe2ca8ca332e7e63e5332a
Julian Caleb Simandjuntak <juliancaleb88@gmail.com>
2023-01-08 17:57:43 +0700

    Update sprites

--- Mixued Feelings/Assets/Scripts/Game/CuttableObject.cs
+++ Mixued Feelings/Assets/Scripts/Game/CuttableObject.cs
@@ -7,10 +7,19 @@ public class CuttableObject : MonoBehaviour
 
     public bool harmful;
     
+    public Sprite newSprite;
+    private SpriteRenderer spriteRenderer;
+    
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D (Collision2D collision) {
         if (collision.gameObject.tag == "Cut") {
-            
-            Destroy(gameObject);
+
+            spriteRenderer.sprite = newSprite;
+
+            GetComponent<Collider2D>().enabled = false;
 
             if (!harmful) {
                 GameObject.Find("ScoreText").transform.GetComponent<ScoreText>().Score += 10;
--- Mixued Feelings/Assets/Scripts/Game/ObjectSpawner.cs
+++ Mixued Feelings/Assets/Scripts/Game/ObjectSpawner.cs
@@ -1,3 +1,44 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class ObjectSpawner : MonoBehaviour
+// {
+//     // Prefab to spawn
+//     public GameObject prefab;
+
+//     // Time and Speed
+//     public float interval;
+//     public float minX, maxX, y;
+
+//     // Sprites
+//     public Sprite[] sprites;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         InvokeRepeating("Spawn", interval, interval);
+//     }
+
+//     private void Spawn() {
+        
+//         // Instantiate
+//         GameObject instance = Instantiate (prefab);
+//         instance.transform.position = new Vector2 (
+//             Random.Range(minX, maxX),
+//             y
+//         );
+//         // Inside Spawner
+//         instance.transform.SetParent(transform);
+
+//         // Select a sprite
+//         Sprite randomSprite = sprites[Random.Range(0, sprites.Length)];
+//         instance.GetComponent<SpriteRenderer>().sprite = randomSprite;
+
+//     }
+    
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +46,11 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     // Prefab to spawn
-    public GameObject prefab;
+    public GameObject[] prefabs;
 
     // Time and Speed
     public float interval;
     public float minX, maxX, y;
-
-    // Sprites
-    public Sprite[] sprites;
 
     // Start is called before the first frame update
     void Start()
@@ -20,21 +58,19 @@ public class ObjectSpawner : MonoBehaviour
         InvokeRepeating("Spawn", interval, interval);
     }
 
-    private void Spawn() {
+    private void Spawn()
+    {
+        // Select a prefab
+        GameObject randomPrefab = prefabs[Random.Range(0, prefabs.Length)];
         
         // Instantiate
-        GameObject instance = Instantiate (prefab);
-        instance.transform.position = new Vector2 (
+        GameObject instance = Instantiate(randomPrefab);
+        instance.transform.position = new Vector2(
             Random.Range(minX, maxX),
             y
         );
-        // Inside Spawner
+
+        // Set the parent of the instantiated object to be the spawner
         instance.transform.SetParent(transform);
-
-        // Select a sprite
-        Sprite randomSprite = sprites[Random.Range(0, sprites.Length)];
-        instance.GetComponent<SpriteRenderer>().sprite = randomSprite;
-
     }
-
 }
